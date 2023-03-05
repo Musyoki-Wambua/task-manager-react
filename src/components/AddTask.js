4b26ebe8ee1e085bea7b073df382c7590cd7e665
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-05 14:44:44 +0300

    AddTask render

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-const AddTask = () => {
+export default function AddTask() {
+
     return (
-        <div>
-            
-        </div>
+        <form className="border p-3 my-3 ">
+          <h1 className="text-center display-4">Add New Task</h1>
+          <div className="form-group">
+            <input type="text" className="form-control" />
+          </div>
+          <button type="submit" className="btn btn-success btn-block">Submit</button>
+        </form>
     );
-};
-
-export default AddTask;
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+import AddTask from './AddTask';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
-   
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto'>
+            <AddTask />
+          </div>
+        </div>
+      </div>   
     </div>
   );
-}
-
-export default App;
+}export default App;
