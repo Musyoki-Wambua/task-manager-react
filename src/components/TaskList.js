7b226836f7265ac5fb271ee418543b0dfa66e70e
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-05 22:16:46 +0300

    Add Tasks render

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,29 +3,32 @@ import React from 'react';
 
 function TaskList() {
     return (
-        <ul className='list-unstyled'>
-            <li className='border p-3'>
-                Name of Task 
-                <button className='btn btn-info float-right mr-2'>Edit</button>
-                <button className='btn btn-danger float-right'>Delete</button>
-            </li>
+      <div>
+        <h1 className='text-center display-4 my-3 '>All Tasks</h1>
+        <ul className="list-unstyled p-3 border">
+          <li className="border p-3">
+            Name of Task
+            <button className="btn btn-danger ">Delete</button>
+            <button className="btn btn-info float-right mr-2">Edit</button>
+          </li>
 
-            <li className='border p-3'>
-                Name of Task 
-                <button className='btn btn-danger float-right mr-2'>Delete</button>
-                <button className='btn btn-info float-right mr-2'>Edit</button>
-            </li>
-            <li className='border p-3'>
-                Name of Task 
-                <button className='btn btn-danger float-right mr-2'>Delete</button>
-                <button className='btn btn-info float-right mr-2'>Edit</button>
-            </li>
-            <li className='border p-3'>
-                Name of Task 
-                <button className='btn btn-danger float-right mr-2'>Delete</button>
-                <button className='btn btn-info float-right mr-2'>Edit</button>
-            </li>      
+          <li className="border p-3">
+            Name of Task
+            <button className="btn btn-danger float-right mr-2">Delete</button>
+            <button className="btn btn-info float-right mr-2">Edit</button>
+          </li>
+          <li className="border p-3">
+            Name of Task
+            <button className="btn btn-danger float-right mr-2">Delete</button>
+            <button className="btn btn-info float-right mr-2">Edit</button>
+          </li>
+          <li className="border p-3">
+            Name of Task
+            <button className="btn btn-danger float-right mr-2">Delete</button>
+            <button className="btn btn-info float-right mr-2">Edit</button>
+          </li>
         </ul>
+      </div>
     );
 }
 export default TaskList;
